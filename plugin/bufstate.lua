2b7f28a051714f3ebc57ef0a46f8a88f019c04a2
André Luís <zandreluisos@gmail.com>
2026-06-05 19:10:15 -0300

    fix: remove stray indentation from top-level if block in plugin.lua

--- plugin/bufstate.lua
+++ plugin/bufstate.lua
@@ -16,12 +16,12 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 -- Default keymaps (disable with vim.g.bufstate_no_default_maps = 1)
-	if not vim.g.bufstate_no_default_maps then
-		vim.keymap.set("n", "<leader>qs", ":BufstateSave<CR>", { desc = "Save session" })
-		vim.keymap.set("n", "<leader>qS", ":BufstateSaveAs<CR>", { desc = "Save session as" })
-		vim.keymap.set("n", "<leader>ql", ":BufstateLoad<CR>", { desc = "Load session" })
-		vim.keymap.set("n", "<leader>qd", ":BufstateDelete<CR>", { desc = "Delete session" })
-		vim.keymap.set("n", "<leader>qn", ":BufstateNew<CR>", { desc = "New session" })
-		vim.keymap.set("n", "<leader>qc", ":BufstateClose<CR>", { desc = "Close workspace" })
-		vim.keymap.set("n", "<leader>qa", ":BufstateAlternate<CR>", { desc = "Alternate session" })
-	end
+if not vim.g.bufstate_no_default_maps then
+	vim.keymap.set("n", "<leader>qs", ":BufstateSave<CR>", { desc = "Save session" })
+	vim.keymap.set("n", "<leader>qS", ":BufstateSaveAs<CR>", { desc = "Save session as" })
+	vim.keymap.set("n", "<leader>ql", ":BufstateLoad<CR>", { desc = "Load session" })
+	vim.keymap.set("n", "<leader>qd", ":BufstateDelete<CR>", { desc = "Delete session" })
+	vim.keymap.set("n", "<leader>qn", ":BufstateNew<CR>", { desc = "New session" })
+	vim.keymap.set("n", "<leader>qc", ":BufstateClose<CR>", { desc = "Close workspace" })
+	vim.keymap.set("n", "<leader>qa", ":BufstateAlternate<CR>", { desc = "Alternate session" })
+end
